0464a540ed85191d38133e2a1334bec2a7be751a
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-02-04 00:58:43 +0200

    refactor register form, configure database connection and created successful and error message

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,22 @@
-//import { NextResponse } from 'next/server';
+import { User } from '@/app/models/User';
+import mongoose from 'mongoose';
 
 export async function POST(req: Request) {
-  return Response.json('ok');
+  try {
+    if (!process.env.MONGODB_URL) {
+      throw new Error('MongoDB URL is not defined in environment variables');
+    }
+    const body = await req.json();
+
+    mongoose.connect(process.env.MONGODB_URL);
+    const createdUser = await User.create(body);
+
+    return Response.json(createdUser);
+  } catch (error) {
+    console.error('Error posting to database:', error);
+    return Response.json(
+      { message: 'Error posting to database' },
+      { status: 500 }
+    );
+  }
 }
--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -3,21 +3,53 @@
 import Image from 'next/image';
 import Google from '../../assets/google.png';
 import { FormEvent, useState } from 'react';
+import Link from 'next/link';
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch('/api/register', {
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch('/api/register', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
+    setCreatingUser(false);
   };
+
   return (
     <section className="mt-8 ">
       <h1 className="text-center text-primary text-4xl">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br />
+          Now you can{' '}
+          <Link className="underline" href="/login">
+            login
+          </Link>
+          .
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occured.
+          <br />
+          Please try again.
+        </div>
+      )}
       <form
         className="block max-w-sm mx-auto mt-4  flex flex-col gap-4"
         onSubmit={handleFormSubmit}
@@ -26,19 +58,28 @@ const Register = () => {
           type="email"
           placeholder="email"
           value={email}
+          disabled={creatingUser}
           onChange={(e) => setEmail(e.target.value)}
-          className="border border-gray-300 px-4 py-2 bg-gray-100 rounded-full"
+          className={`border border-gray-300 px-4 py-2 rounded-full ${
+            creatingUser ? 'pointer-events-none opacity-50' : ''
+          }`}
         />
         <input
           type="password"
           placeholder="password"
           value={password}
+          disabled={creatingUser}
           onChange={(e) => setPassword(e.target.value)}
-          className="border border-gray-300 px-4 py-2 bg-gray-100 rounded-full"
+          className={`border border-gray-300 px-4 py-2 rounded-full ${
+            creatingUser ? 'pointer-events-none opacity-50' : ''
+          }`}
         />
         <button
           type="submit"
-          className="bg-primary text-white p-2 rounded-full"
+          className={`bg-primary text-white p-2 rounded-full ${
+            creatingUser ? 'pointer-events-none opacity-50' : ''
+          }`}
+          disabled={creatingUser}
         >
           Register
         </button>
@@ -47,6 +88,12 @@ const Register = () => {
           <Image src={Google} width={15} height={15} alt="google" />
           Login with Google
         </button>
+        <div className="text-center mt-4">
+          Existing account?
+          <Link className="underline" href={'/login'}>
+            Login
+          </Link>
+        </div>
       </form>
     </section>
   );
